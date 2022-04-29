ed70fdf087d97e130e00ca3418688eef175bf239
Ole Tjernstad <ott556@gmail.com>
2022-04-29 22:53:25 +0200

    test custom menubar

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -6,12 +6,33 @@ import { FileContextProvider } from './context/file';
 
 export default function App() {
   return (
-    <FileContextProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<EditorScreen />} />
-        </Routes>
-      </Router>
-    </FileContextProvider>
+    <>
+      <div id="menu-bar">
+        <div className="left" role="menu">
+          <button type="button" className="menubar-btn" id="menu-btn">
+            |||
+          </button>
+          <h5>My Awesome App</h5>
+        </div>
+        <div className="right">
+          <button type="button" className="menubar-btn" id="minimize-btn">
+            -
+          </button>
+          <button type="button" className="menubar-btn" id="max-unmax-btn">
+            O
+          </button>
+          <button type="button" className="menubar-btn" id="close-btn">
+            X
+          </button>
+        </div>
+      </div>
+      <FileContextProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<EditorScreen />} />
+          </Routes>
+        </Router>
+      </FileContextProvider>
+    </>
   );
 }
