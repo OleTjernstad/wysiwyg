7c1629dc87f830a36313a0f735fe1c835919074e
Ole Tjernstad <ott556@gmail.com>
2022-06-02 22:04:09 +0200

    added logo on background

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -4,12 +4,20 @@ import { Route, MemoryRouter as Router, Routes } from 'react-router-dom';
 import EditorScreen from './screens/editor-screen';
 import { FileContextProvider } from './context/file';
 import AppBar from './components/app-bar';
+import logo from '../../assets/logo.png';
 
 export default function App() {
   return (
     <>
       <Toaster />
       <AppBar />
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <img
+          src={logo}
+          style={{ height: '40vmin', marginTop: '5vh' }}
+          alt="logo"
+        />
+      </div>
       <FileContextProvider>
         <Router>
           <Routes>
