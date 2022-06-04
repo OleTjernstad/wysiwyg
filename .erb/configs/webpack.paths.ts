49b5a3e2e9688e15642226b7eb4773e6c3379440
Ole Tjernstad <ott556@gmail.com>
2022-06-04 23:03:15 +0200

    prepare for publish

--- .erb/configs/webpack.paths.ts
+++ .erb/configs/webpack.paths.ts
@@ -7,7 +7,7 @@ const dllPath = path.join(__dirname, '../dll');
 const srcPath = path.join(rootPath, 'src');
 const srcMainPath = path.join(srcPath, 'main');
 const srcRendererPath = path.join(srcPath, 'renderer');
-const srcContractPath = path.join(srcPath, 'contracts');
+const srcAppxPath = path.join(rootPath, 'appx');
 
 const releasePath = path.join(rootPath, 'release');
 const appPath = path.join(releasePath, 'app');
@@ -27,7 +27,7 @@ export default {
   srcPath,
   srcMainPath,
   srcRendererPath,
-  srcContractPath,
+  srcAppxPath,
   releasePath,
   appPath,
   appPackagePath,
